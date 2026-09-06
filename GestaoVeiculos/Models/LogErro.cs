41641a0668237c56516caf2d3d0a84af7cc88821
abnerberg10@gmail.com <abnerberg10@gmail.com>
2026-09-05 22:53:58 -0300

    feat: adiciona modelo LogErro para auditoria

---
+++ GestaoVeiculos/Models/LogErro.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GestaoVeiculos.Models;
+
+public partial class LogErro
+{
+    public int Id { get; set; }
+
+    public DateTime DataHora { get; set; }
+
+    public string Mensagem { get; set; } = null!;
+
+    public string LocalErro { get; set; } = null!;
+
+    public string? CodigoErro { get; set; }
+
+    public string? RastroCodigo { get; set; }
+}
