7731e44175e0d5f1695bb6fe50bb6940c0c7aa1c
abnerberg10@gmail.com <abnerberg10@gmail.com>
2026-09-05 22:55:56 -0300

    feat: configura DbSets e altera mapeamento EF Core para Marca, LogErro e LogTransacao

--- GestaoVeiculos/Data/AppDbContext.cs
+++ GestaoVeiculos/Data/AppDbContext.cs
@@ -21,6 +21,8 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<Marca> Marcas { get; set; }
 
     public virtual DbSet<Veiculo> Veiculos { get; set; }
+    public virtual DbSet<LogErro> LogsErro { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -79,11 +81,12 @@ public partial class AppDbContext : DbContext
                 .HasMaxLength(50)
                 .HasColumnName("tipo_veiculo");
 
-            entity.HasOne(d => d.Marca).WithMany(p => p.Veiculos)
+            entity.HasOne(d => d.Marca).WithMany()
                 .HasForeignKey(d => d.MarcaId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("fk_veiculo_marca");
         });
+
 
         OnModelCreatingPartial(modelBuilder);
     }
