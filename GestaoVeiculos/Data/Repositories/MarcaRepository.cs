087d736b2e727b1c9271a030a8b14a3cc738eddb
abnerberg10@gmail.com <abnerberg10@gmail.com>
2026-09-08 16:30:44 -0300

    refactor(MarcaRepository): adiciona SaveChanges() para persistir e limpar rastreamento do EF Core

--- GestaoVeiculos/Data/Repositories/MarcaRepository.cs
+++ GestaoVeiculos/Data/Repositories/MarcaRepository.cs
@@ -11,27 +11,66 @@ namespace GestaoVeiculos.Data.Repositories
     {
         private readonly AppDbContext _contexto;
 
+        public MarcaRepository(AppDbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
         public void Alterar(Marca marca)
         {
-            _contexto.Marcas.Update(marca);
-            _contexto.SaveChanges();
+            try
+            {
+                var marcaExistente = _contexto.Marcas.Find(marca.Id);
+                if (marcaExistente != null)
+                {
+                    marcaExistente.Nome = marca.Nome;
+                    _contexto.SaveChanges();
+                }
+            }
+            finally
+            {
+                _contexto.ChangeTracker.Clear();
+            }
         }
 
         public void Cadastrar(Marca marca)
         {
-            _contexto.Marcas.Add(marca);
-            _contexto.SaveChanges();
+            try
+            {
+                _contexto.Marcas.Add(marca);
+                _contexto.SaveChanges();
+            }
+            finally
+            {
+                _contexto.ChangeTracker.Clear();
+            }
         }
 
-        public void Excluir(Marca marca)
+        public void Excluir(int marcaId)
         {
-            _contexto.Marcas.Remove(marca);
-            _contexto.SaveChanges();
+            try
+            {
+                var marca = _contexto.Marcas.Find(marcaId);
+                if (marca != null)
+                {
+                    _contexto.Marcas.Remove(marca);
+                    _contexto.SaveChanges();
+                }
+            }
+            finally
+            {
+                _contexto.ChangeTracker.Clear();
+            }
         }
 
         public IEnumerable<Marca> ListarTodos()
         {
             return _contexto.Marcas.AsNoTracking().ToList();
+        }
+
+        public bool NomeIgualAtual(Marca marca)
+        {
+            return _contexto.Marcas.Any(n => n.Nome == marca.Nome && n.Id == marca.Id);
         }
     }
 }
